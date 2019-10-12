f8e688242bf13fd6ecab2c07de5454ba80338de2
dengkai <dkai_2012@163.com>
2019-10-12 18:47:10 +0800

    增加C模块编写Demo

---
+++ lua_test/C_module_test/module.c
@@ -0,0 +1,41 @@
+/*
+ * 实现供Lua调用的C模块
+ */
+#include "lua.h"
+#include "lauxlib.h"
+
+static int module_add(lua_State *L)
+{
+	double arg1 = luaL_checknumber(L, 1);
+	double arg2 = luaL_checknumber(L, 2);
+
+	double ret = arg1 + arg2;
+	lua_pushnumber(L, ret);
+
+	return 1;
+}
+
+static int module_sub(lua_State *L)
+{
+	double arg1 = luaL_checknumber(L, 1);
+	double arg2 = luaL_checknumber(L, 2);
+
+	double ret = arg1 - arg2;
+	lua_pushnumber(L, ret);
+
+	return 1;
+}
+
+LUA_API int luaopen_skynet_core_aa(lua_State *L)
+{
+	luaL_Reg l[] = {
+		{ "madd", module_add },
+		{ "msub", module_sub },
+		{ NULL, NULL},
+	};
+
+	lua_createtable(L, 0, sizeof(l)/sizeof(l[0]) - 1);
+	luaL_setfuncs(L, l, 0);
+
+	return 1;
+}
---
+++ lua_test/C_module_test/test.lua
@@ -0,0 +1,4 @@
+local c = require "skynet.core_aa"
+
+print(c.madd(1.1, 4))
+print(c.msub(5, 1.2))
