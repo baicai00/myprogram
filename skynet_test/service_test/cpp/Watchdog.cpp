c040ef85c0e235b408b6b64d41280ccefec27924
dik <dik.deng@kiwigame.com>
2020-10-03 20:02:49 +0800

    commit

---
+++ skynet_test/service_test/cpp/Watchdog.cpp
@@ -0,0 +1,36 @@
+#include "Watchdog.h"
+#include <string.h>
+
+Watchdog::Watchdog()
+{
+
+}
+
+bool Watchdog::dog_init(const string& parm)
+{
+    return true;
+}
+
+void Watchdog::watchdog_poll(const char* data, uint32_t size, uint32_t source, int session, int type)
+{
+    switch (type)
+    {
+    case PTYPE_TEXT:
+    case PTYPE_CLIENT:
+    {
+        char* c = (char*)data;
+        int fd = atoi(strsep(&c, " "));
+        //LOG(INFO) << "PTYPE_CLIENT fd = " << fd;
+        printf("watchdog_poll fd=%d", fd);
+        break;
+    }
+    case PTYPE_RESPONSE:
+    default:
+        break;
+    }
+}
+
+bool Watchdog::service_init(skynet_context* ctx, const void* parm, int len)
+{
+    return true;
+}
